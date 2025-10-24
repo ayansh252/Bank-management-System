5e633593cc7ddf086d48c4a61aa169b65a048685
Ayansh252yadav <ayanshy652@gmail.com>
2025-10-25 01:23:31 +0530

    new File

---
+++ src/bank/management/system/withdrawl.java
@@ -0,0 +1,110 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.util.Date;
+
+public class withdrawl extends JFrame implements ActionListener {
+
+    String pin;
+    TextField textField;
+    JButton b1,b2;
+
+    withdrawl(String pin){
+        this.pin=pin;
+
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icon/atm2.jpeg"));
+        Image i2=i1.getImage().getScaledInstance(1550,830, Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel l3=new JLabel(i3);
+        l3.setBounds(0,0,1550,800);
+        add(l3);
+
+        JLabel label1=new JLabel("MAXIMUM WITHDRAWL RS.25000");
+        label1.setFont(new Font("Raleway",Font.BOLD,16));
+        label1.setBounds(460,180,700,35);
+        label1.setForeground(Color.WHITE);
+        l3.add(label1);
+
+        JLabel label2=new JLabel("PLEASE ENTER YOUR AMOUNT");
+        label2.setFont(new Font("Raleway",Font.BOLD,16));
+        label2.setBounds(460,220,400,35);
+        label2.setForeground(Color.WHITE);
+        l3.add(label2);
+
+        textField=new TextField();
+        textField.setBounds(460,260,320,25);
+        textField.setBackground(new Color(65,125,128));
+        textField.setForeground(Color.WHITE);
+        textField.setFont(new Font("Raleway",Font.BOLD,22));
+        l3.add(textField);
+
+        b1=new JButton("WITHDRAW");
+        b1.setBounds(700,348,150,35);
+        b1.setBackground(new Color(65,125,128));
+        b1.setForeground(Color.WHITE);
+        b1.addActionListener(this);
+        l3.add(b1);
+
+        b2=new JButton("BACK");
+        b2.setForeground(Color.WHITE);
+        b2.setBackground(new Color(65,125,128));
+        b2.setBounds(700,395,150,35);
+        b2.addActionListener(this);
+        l3.add(b2);
+
+
+
+        setLayout(null);
+        setSize(1550,1080);
+        setLocation(0,0);
+        setVisible(true);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == b1) {
+            try {
+                String amount = textField.getText();
+                Date date = new Date();
+                if (textField.getText().equals("")) {
+                    JOptionPane.showMessageDialog(null, "Please enter the amount you want to withdraw");
+                } else {
+                    conicon1 c = new conicon1();
+                    ResultSet resultSet = c.statement.executeQuery("Select * from bank where pin='" + pin + "'");
+                    int balance = 0;
+                    while (resultSet.next()) {
+                        if (resultSet.getString("type").equals("Deposit")) {
+                            balance += Integer.parseInt(resultSet.getString("amount"));
+                        } else {
+                            balance -= Integer.parseInt(resultSet.getString("amount"));
+                        }
+                    }
+                    if (balance < Integer.parseInt(amount)) {
+                        JOptionPane.showMessageDialog(null, "Insufficient balance");
+                        return;
+                    }
+
+                    c.statement.executeUpdate("insert into bank values('" + pin + "','" + date + "','withdrawl','" + amount + "')");
+                    JOptionPane.showMessageDialog(null, "Rs. " + amount + "Debited successfull");
+                    setVisible(false);
+                    new main_class(pin);
+                }
+            } catch (Exception E) {
+
+            }
+        }else if(e.getSource()==b2){
+            setVisible(false);
+            new main_class(pin);
+        }
+    }
+
+    public static void main(String[] args) {
+        new withdrawl("");
+    }
+}
